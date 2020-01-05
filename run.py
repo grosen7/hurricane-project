c7a61614c18fa9e578f158e5c7748f5d03597b82
Gabe Rosen <>
2020-01-04 20:45:42 -0500

    added a few print statements

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from classes.FloridaHurricaneFind import FloridaHurricaneFind
+from datetime import datetime
 
 if __name__ == "__main__":
+    print("Running FloridaHurricaneFind program")
+    start = datetime.now()
     FloridaHurricaneFind().floridaHurricaneFind("data/hurdat2.txt")
+    print("Script completed in {} seconds".format(datetime.now()-start))
